7d69484920b528d2badbaba04da36be229de1993
Khalil Depeazer <khalildepeazer@gmail.com>
2026-07-16 15:35:04 -0500

    Add session-based shopping cart with quantity update and remove (features 3.1.1, 3.1.2, 4.2.3)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  resource :cart, only: [:show]
+  post '/cart/add/:product_id', to: 'cart#add', as: 'add_to_cart'
+  delete '/cart/remove/:product_id', to: 'cart#remove', as: 'remove_from_cart'
+  patch '/cart/update/:product_id', to: 'cart#update', as: 'update_cart'
+  get "cart/show"
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
