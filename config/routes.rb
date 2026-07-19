f8079e13efbc1b81194982c8aec487d0841be48b
Khalil Depeazer <khalildepeazer@gmail.com>
2026-07-18 22:40:32 -0500

    Add category index and show pages for browsing products by category (feature 2.2)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get "categories/index"
+  get "categories/show"
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
@@ -6,6 +8,7 @@ Rails.application.routes.draw do
 
   resources :products, only: [:index, :show]
   resource :cart, only: [:show]
+  resources :categories, only: [:index, :show]
   post '/cart/add/:product_id', to: 'carts#add', as: 'add_to_cart'
   delete '/cart/remove/:product_id', to: 'carts#remove', as: 'remove_from_cart'
   patch '/cart/update/:product_id', to: 'carts#update', as: 'update_cart'
