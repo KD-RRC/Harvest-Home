052cf454844ff77c3afb14d9232fdb78315e3d98
Khalil Depeazer <khalildepeazer@gmail.com>
2026-07-28 19:30:27 -0500

    Add search highlight helper method

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -1,2 +1,6 @@
 module SearchHelper
+  def highlight_query(text, query)
+    return text if query.blank?
+    text.to_s.gsub(/#{Regexp.escape(query)}/i) { |match| "<mark>#{match}</mark>" }.html_safe
+  end
 end
